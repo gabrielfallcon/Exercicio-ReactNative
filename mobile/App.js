ad34b8fd4c8b85a0a5183fbeb1f7813957ff19a4
Gabriel Nascimento <gabriel.nascimenton.19@gmail.com>
2020-03-18 00:23:00 -0300

    criando a funcao de gerar numeros aleatorios

--- mobile/App.js
+++ mobile/App.js
@@ -1,16 +1,29 @@
 import React, {useState} from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button } from 'react-native';
 
 export default function App() {
-  const [outputText, setOutputText] = useState("Texto inicial")
-  const [contador, setContador] = useState(0)
+  const [aleatorio, setAleatorio] = useState([null])
+  
+  
+  const gerarAleatorio = () => {
+    for(let i = 0; i<= 6; i++){
+      
+      let resultado = Math.floor(Math.random() * 60)
+      setAleatorio([...aleatorio, resultado])
+    }
+  }
+  const sorte = aleatorio.map((item) => item)
+
+  const zerarNumeros = () =>{
+    setAleatorio([null])
+  }
+  
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
-      <Text>{outputText}</Text>
-      <Button title="Alterar texto" onPress={() => setOutputText("Texto novo")}/>
-      <Text>{contador}</Text>
-      <Button title="Alterar texto" onPress={() => setContador(contador + 1)} />
+      <Text>{sorte}</Text>
+      <Button title="Gerar numero da sorte" onPress={() => gerarAleatorio()}/>
+      <Button title="Zerar jogo" onPress={() => zerarNumeros()}/>
     </View>
   );
 }
